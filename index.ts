69472ab41b27dea0a67fdbc4587c2b664218899b
Benoit Serrano <benoit.serrano10@gmail.com>
2021-03-07 13:13:07 +0100

    fetch appointments in vaccination center

---
+++ index.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { config } from "dotenv";
+
+function main() {
+  config();
+
+  fetchAvailabilitiesFromDoctolib();
+}
+
+async function fetchAvailabilitiesFromDoctolib() {
+  console.log(process.env.CENTER_ID);
+  const doctolibRequest = buildDoctolibRequest();
+  const result = await axios.get(doctolibRequest);
+}
+
+function buildDoctolibRequest() {
+  const CENTER_ID = 5511046;
+  const COVID_SPECIALITY_ID = 5494;
+  return `https://www.doctolib.fr/search_results/${CENTER_ID}.json?limit=6&speciality_id=${COVID_SPECIALITY_ID}&search_result_format=json`;
+}
+
+main();
